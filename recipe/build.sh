c2dcff873f3e9f1eeb27779fccdc8ae5248a8ce3
Nishidha <npanpa23@in.ibm.com>
2021-01-18 12:22:05 +0000

    Updated copyright information

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,6 @@
-# (C) Copyright IBM Corp. 2018, 2020. All Rights Reserved.
+#!/bin/bash
+# *****************************************************************
+# (C) Copyright IBM Corp. 2020, 2021. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,9 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/bash
-
+# *****************************************************************
 set -vex
 
 
