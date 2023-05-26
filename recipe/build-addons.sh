1c1de625c90ce90b17d8d1fcdbb35069180876e3
ArchanaShinde1 <106796026+ArchanaShinde1@users.noreply.github.com>
2023-05-26 14:41:06 +0530

    call common function for bazel shutdown (#35)
    * call common function for bazel shutdown
    
    * update copyright year
    
    ---------
    
    Co-authored-by: ArchanaShinde1 <Archana@Shinde2504@gmail.com>

--- recipe/build-addons.sh
+++ recipe/build-addons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # *****************************************************************
-# (C) Copyright IBM Corp. 2020, 2022. All Rights Reserved.
+# (C) Copyright IBM Corp. 2020, 2023. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,8 +17,7 @@
 set -vex
 
 
-bazel clean --expunge
-bazel shutdown
+source open-ce-common-utils.sh
 
 if [[ $build_type == "cuda" ]];
 then 
@@ -49,5 +48,6 @@ pip install --no-deps $SRC_DIR/tensorflow_addons_pkg/*.whl
 echo "PREFIX: $PREFIX"
 echo "RECIPE_DIR: $RECIPE_DIR"
 
-bazel clean --expunge
-bazel shutdown
+PID=$(bazel info server_pid)
+echo "PID: $PID"
+cleanup_bazel $PID
